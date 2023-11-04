b0701ef754250c4dc1cb6c2cdc8011c173b4dde8
tgharry50 <127186125+tgharry50@users.noreply.github.com>
2023-11-04 15:04:47 +0100

    Update addition_teacher.py

--- src/addition_teacher.py
+++ src/addition_teacher.py
@@ -1,5 +1,7 @@
-check_result_of_addition(a:int, b:int, result:int):
+def check_result_of_addition(a:int, b:int, result:int):
     if a + b == result:
         print("Wynik poprawny")
     else:
         print("Błąd dodawania")
+
+check_result_of_addition(1,2,3)
